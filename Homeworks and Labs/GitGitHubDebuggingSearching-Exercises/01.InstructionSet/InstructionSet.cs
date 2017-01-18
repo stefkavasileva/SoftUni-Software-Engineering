9d2a58628f69149d188d79cda6c6ac4fbc074a59
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-01-18 18:44:52 +0200

    Debugging- exercises

---
+++ Homeworks and Labs/GitGitHubDebuggingSearching-Exercises/01.InstructionSet/InstructionSet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class InstructionSet
+{
+    static void Main(string[] args)
+    {
+        string opCode = Console.ReadLine();
+        long result = 0;
+
+        while (opCode != "END")
+        {
+            string[] codeArgs = opCode.Split(' ');
+
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = operandOne + 1;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = operandOne - 1;
+                        break;
+                    }
+                case "ADD":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne * operandTwo;
+                        break;
+                    }
+            }
+            Console.WriteLine(result);
+            opCode = Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ Homeworks and Labs/GitGitHubDebuggingSearching-Exercises/04.Substring/Substring.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Substring
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        const char Search = 'p';
+        bool hasMatch = false;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == Search)
+            {
+                hasMatch = true;
+
+                int endIndex = jump+i;
+
+                if (endIndex >= text.Length)
+                {
+                    jump = text.Length-1-i;
+                }
+
+               string matchedString = text.Substring(i,jump+1);
+                Console.WriteLine(matchedString);
+                i += jump;
+            }
+        }
+
+        if (!hasMatch)
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
+
