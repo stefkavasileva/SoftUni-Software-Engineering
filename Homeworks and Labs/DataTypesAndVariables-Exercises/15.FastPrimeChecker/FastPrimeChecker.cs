98498d83ceba10f9ce0c6804d04ab5311da443c4
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-01-19 19:52:24 +0200

    Edit all exercises

---
+++ Homeworks and Labs/DataTypesAndVariables-Exercises/15.FastPrimeChecker/FastPrimeChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class FastPrimeChecker
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        for (int index = 2; index <= num; index++)
+        {
+            bool isPrime = true;
+            for (int i = 2; i <= Math.Sqrt(index); i++)
+            {
+                if (index % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            Console.WriteLine($"{index} -> {isPrime}");
+        }
+
+    }
+}
+
