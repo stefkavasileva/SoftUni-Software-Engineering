80b60777c0fff3d4614a45edc438b4d72a7070b1
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-01-21 13:40:07 +0200

    Old exam, programming fundamentals

---
+++ OldExams/(Old)Exam-24April2016/02.GameOfNames/GameOfNames.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class GameOfNames
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int score = 0;
+        int bestScore = int.MinValue;
+        string nameOfWinner = string.Empty;
+
+        for (int i = 0; i < n; i++)
+        {
+            string name = Console.ReadLine();
+            int initialScore = int.Parse(Console.ReadLine());
+            score += initialScore;
+            for (int j = 0; j < name.Length; j++)
+            {
+                if (name[j] % 2 == 0)
+                {
+                    score += name[j];
+                }
+                else
+                {
+                    score -= name[j];
+                }
+            }
+
+            if (score > bestScore)
+            {
+                bestScore = score;
+                nameOfWinner = name;
+            }
+
+            score = 0;
+        }
+
+        Console.WriteLine("The winner is {0} - {1} points", nameOfWinner, bestScore);
+    }
+}
+
