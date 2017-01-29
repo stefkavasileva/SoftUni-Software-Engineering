3c35a8b0d57ecedef6b73ba4eff234afaa7eb68a
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-01-29 20:41:22 +0200

    Update exercise.

--- OldExams/RetakeExam-11September2016/03.Portal/Portal.cs
+++ OldExams/RetakeExam-11September2016/03.Portal/Portal.cs
@@ -20,130 +20,102 @@ class Portal
         int row = startPosition[0];
         int col = startPosition[1];
         int turns = 0;
-        for (;;)
-        {
 
-            if (matrix[row][col] == 'E')
+        for (int i = 0; i < diraction.Length; i++)
+        {
+            turns++;
+            if (diraction[i].Equals('D'))
+            {
+                row++;
+                if (row >= matrix.Length)
+                {
+                    row = 0;
+                }
+
+                if (matrix[row][col] == 'X')
+                {
+                    while (matrix[row][col] == 'X')
+                    {
+                        row++;
+                        if (row >= matrix.Length)
+                        {
+                            row = 0;
+                        }
+                    }
+                }
+
+            }
+            else if (diraction[i].Equals('U'))
+            {
+                row--;
+                if (row == -1)
+                {
+                    row = matrix.Length - 1;
+                }
+
+                if (matrix[row][col] == 'X')
+                {
+                    while (matrix[row][col] == 'X')
+                    {
+                        row--;
+                        if (row == -1)
+                        {
+                            row = matrix.Length - 1;
+                        }
+                    }
+                }
+            }
+            else if (diraction[i].Equals('L'))
+            {
+                col--;
+                if (col == -1)
+                {
+                    col = matrix[row].Length - 1;
+                }
+
+                if (matrix[row][col] == 'X')
+                {
+                    while (matrix[row][col] == 'X')
+                    {
+                        col--;
+                        if (col == -1)
+                        {
+                            col = matrix[row].Length - 1;
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+
+                col++;
+                if (col >= matrix[row].Length)
+                {
+                    col = 0;
+                }
+
+                if (matrix[row][col] == 'X')
+                {
+                    while (matrix[row][col] == 'X')
+                    {
+                        col++;
+                        if (col >= matrix[row].Length)
+                        {
+                            col = 0;
+                        }
+                    }
+                }
+            }
+
+            if (matrix[row][col].Equals('E'))
             {
                 Console.WriteLine($"Experiment successful. {turns} turns required.");
                 return;
             }
-
-            if (turns.Equals(diraction.Length))
-            {
-                Console.WriteLine("Robot stuck at {0} {1}. Experiment failed.", row, col);
-                return;
-            }
-
-            if (diraction[turns].Equals('D'))
-            {
-                while (true)
-                {
-                    row++;
-                    if (row > size - 1)
-                    {
-                        row = 0;
-                    }
-
-                    if (matrix[row][col] == 'E')
-                    {
-                        turns++;
-                        Console.WriteLine($"Experiment successful. {turns} turns required.");
-                        return;
-                    }
-
-                    if (matrix[row][col] == 'X')
-                    {
-
-                        continue;
-                    }
-
-                    break;
-                }
-            }
-            else if (diraction[turns].Equals('U'))
-            {
-                while (true)
-                {
-                    row--;
-                    if (row < 0)
-                    {
-                        row = size - 1;
-                    }
-
-                    if (matrix[row][col] == 'E')
-                    {
-                        turns++;
-                        Console.WriteLine($"Experiment successful. {turns} turns required.");
-                        return;
-                    }
-                    if (matrix[row][col] == 'X')
-                    {
-                        continue;
-                    }
-                    break;
-                }
-
-
-            }
-            else if (diraction[turns].Equals('R'))
-            {
-                while (true)
-                {
-                    col++;
-                    if (col > size - 1)
-                    {
-                        col = 0;
-                    }
-
-                    if (matrix[row][col] == 'E')
-                    {
-                        turns++;
-                        Console.WriteLine($"Experiment successful. {turns} turns required.");
-                        return;
-                    }
-
-                    if (matrix[row][col] == 'X')
-                    {
-                        continue;
-                    }
-                    break;
-
-                }
-
-
-            }
-            else if (diraction[turns].Equals('L'))
-            {
-                while (true)
-                {
-                    col--;
-                    if (col < 0)
-                    {
-                        col = size - 1;
-                    }
-
-                    if (matrix[row][col] == 'E')
-                    {
-                        turns++;
-                        Console.WriteLine($"Experiment successful. {turns} turns required.");
-                        return;
-                    }
-
-                    if (matrix[row][col] == 'X')
-                    {
-                        continue;
-                    }
-                    break;
-                }
-
-
-            }
-
-            turns++;
-
         }
 
+        Console.WriteLine($"Robot stuck at {row} {col}. Experiment failed.");
 
     }
 
