9b582ed9772dd85fdb599af4bb81d313cb628f39
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-01-30 22:15:53 +0200

    Add exercise.

---
+++ Homeworks and Labs/Lists/02.AppendLists/AppendLists.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AppendLists
+{
+    public static void Main()
+    {
+        char[] delimiter = " ".ToCharArray();
+
+        List<string> inputLists = Console.ReadLine()
+            .Split('|')
+            .Reverse()
+            .ToList();
+
+        List<string> reversedElements = new List<string>();
+
+        for (int i = 0; i < inputLists.Count; i++)
+        {
+            List<string> tempElements = inputLists[i]
+                .Split(delimiter, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            reversedElements.AddRange(tempElements);
+        }
+
+        Console.WriteLine(string.Join(" ", reversedElements));
+    }
+}
