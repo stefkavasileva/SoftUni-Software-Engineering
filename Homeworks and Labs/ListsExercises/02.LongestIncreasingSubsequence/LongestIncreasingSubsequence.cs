421dda87400773792cf3195ea4e74499b2bb8222
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-01-31 20:29:01 +0200

    Add exercises.

---
+++ Homeworks and Labs/ListsExercises/02.LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LongestIncreasingSubsequence
+{
+    public static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        List<int> len = new List<int>();
+        List<int> prev = new List<int>();
+    }
+}
+
---
+++ Homeworks and Labs/ListsExercises/05.BombNumbers/BombNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BombNumbers
+{
+    public static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int[] bombArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int bombNumber = bombArgs[0];
+        int power = bombArgs[1];
+
+        int indexOfBombNum = numbers.IndexOf(bombNumber);
+
+        while (indexOfBombNum != -1)
+        {
+            int leftPower = indexOfBombNum < power ? indexOfBombNum : power;
+            int leftIndex = indexOfBombNum - power < 0 ? 0 : indexOfBombNum - power;
+            int rightPower = indexOfBombNum + power > numbers.Count ? numbers.Count - indexOfBombNum - 1 : power;
+
+            numbers.RemoveRange(leftIndex, leftPower + rightPower + 1);
+            indexOfBombNum = numbers.IndexOf(bombNumber);
+        }
+
+        Console.WriteLine(numbers.Sum());
+    }
+}
