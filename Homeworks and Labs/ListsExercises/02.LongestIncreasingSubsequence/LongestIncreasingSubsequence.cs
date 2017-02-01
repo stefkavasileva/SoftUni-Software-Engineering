ad7275244cdeca5aa7c4b6eff64a444858777a04
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-01 11:53:36 +0200

    Add exercise.

--- Homeworks and Labs/ListsExercises/02.LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
+++ Homeworks and Labs/ListsExercises/02.LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
@@ -11,8 +11,42 @@ public class LongestIncreasingSubsequence
             .Select(int.Parse)
             .ToList();
 
-        List<int> len = new List<int>();
-        List<int> prev = new List<int>();
+        int[] len = new int[numbers.Count];
+        int[] prev = new int[numbers.Count];
+
+        int maxLength = 0;
+        int lastIndex = -1;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            len[i] = 1;
+            prev[i] = -1;
+            for (int j = 0; j <= i; j++)
+            {
+                if (numbers[j] < numbers[i] && len[j] + 1 > len[i])
+                {
+                    len[i] = len[j] + 1;
+                    prev[i] = j;
+                }
+
+                if (len[i] > maxLength)
+                {
+                    maxLength = len[i];
+                    lastIndex = i;
+                }
+            }
+        }
+
+        List<int> longesSubSeq = new List<int>();
+
+        while (lastIndex != -1)
+        {
+            longesSubSeq.Add(numbers[lastIndex]);
+            lastIndex = prev[lastIndex];
+        }
+
+        longesSubSeq.Reverse();
+
+        Console.WriteLine(string.Join(" ", longesSubSeq));
     }
 }
-
