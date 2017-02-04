f57832332a2754b2efe46d9333bfd1ec016e52f2
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-04 14:05:34 +0200

    Add exercises.

---
+++ Programming Fundamentals - Extended - Exercises/DataTypesTextOtherTypesLab/04.RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class RefactorVolumeOfPyramid
+{
+    public static void Main()
+    {
+        Console.Write("Length: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("Width: ");
+        double width = double.Parse(Console.ReadLine());
+
+        Console.Write("Height: ");
+        double height = double.Parse(Console.ReadLine());
+
+        double volume = (length * width * height) / 3.0;
+
+        Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+    }
+}
