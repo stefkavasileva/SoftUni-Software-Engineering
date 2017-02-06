87a90616fbf1fb7d32096c6bd6f8efe454d29b83
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-06 16:26:51 +0200

    Add exercise.

--- Homeworks and Labs/ObjectsAndClasses/05.ClosestTwoPoints/ClosestTwoPoints.cs
+++ Homeworks and Labs/ObjectsAndClasses/05.ClosestTwoPoints/ClosestTwoPoints.cs
@@ -1,57 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-class Point
+public class Point
 {
     public double X { get; set; }
+
     public double Y { get; set; }
 
-    public static double CalDistance(Point p1, Point p2)
+    public override string ToString()
     {
-        double sideA = p1.X - p2.X;
-        double sideB = p1.Y - p2.Y;
+        return string.Format("({0}, {1})", this.X, this.Y);
+    }
+}
+
+public class ClosestTwoPoints
+{
+    public static void Main()
+    {
+        Point[] points = ReadsArrayOfPoints();
+        Point[] closestPoints = FindClosestPoints(points);
+
+        double minDistance = CalDistance(closestPoints[0], closestPoints[1]);
+
+        Console.WriteLine($"{minDistance:f3}");
+
+        foreach (var point in closestPoints)
+        {
+            Console.WriteLine(point);
+        }
+    }
+
+    public static double CalDistance(Point firstPoint, Point seconPoint)
+    {
+        double sideA = firstPoint.X - seconPoint.X;
+        double sideB = firstPoint.Y - seconPoint.Y;
+
         double distance = Math.Sqrt((sideA * sideA) + (sideB * sideB));
+
         return distance;
     }
 
     public static Point ReadPoint()
     {
         double[] coordinates = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
         Point point = new Point { X = coordinates[0], Y = coordinates[1] };
+
         return point;
     }
 
     public static Point[] ReadsArrayOfPoints()
     {
         int sizeOfArray = int.Parse(Console.ReadLine());
+
         Point[] points = new Point[sizeOfArray];
 
         for (int p = 0; p < sizeOfArray; p++)
         {
-            points[p] = Point.ReadPoint();
+            points[p] = ReadPoint();
         }
+
         return points;
     }
 
     public static Point[] FindClosestPoints(Point[] points)
     {
         Point[] closestPoints = new Point[2];
+
         double minDistance = double.MaxValue;
 
-        for (int firstP = 0; firstP < points.Length; firstP++)
+        for (int i = 0; i < points.Length; i++)
         {
-            for (int secondP = firstP + 1; secondP < points.Length; secondP++)
+            for (int j = i + 1; j < points.Length; j++)
             {
-                Point p1 = points[firstP];
-                Point p2 = points[secondP];
-                double distance = Point.CalDistance(p1, p2);
+                Point firstPoint = points[i];
+                Point secondPoint = points[j];
+
+                double distance = CalDistance(firstPoint, secondPoint);
+
                 if (distance < minDistance)
                 {
-                    closestPoints[0] = p1;
-                    closestPoints[1] = p2;
+                    closestPoints[0] = firstPoint;
+                    closestPoints[1] = secondPoint;
                     minDistance = distance;
                 }
             }
@@ -59,27 +89,4 @@ class Point
 
         return closestPoints;
     }
-
-    public override string ToString()
-    {
-        return string.Format("({0}, {1})", this.X, this.Y);
-    }
-
 }
-
-class ClosestTwoPoints
-{
-    static void Main(string[] args)
-    {
-        Point[] points = Point.ReadsArrayOfPoints();
-        Point[] closestPoints = Point.FindClosestPoints(points);
-        double minDistance = Point.CalDistance(closestPoints[0], closestPoints[1]);
-
-        Console.WriteLine("{0:f3}", minDistance);
-        foreach (var point in closestPoints)
-        {
-            Console.WriteLine(point);
-        }
-    }
-}
-
