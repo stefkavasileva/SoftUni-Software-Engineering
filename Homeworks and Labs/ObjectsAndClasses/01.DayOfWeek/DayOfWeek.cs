7832b349673c86900c41c94e1ca5650964976b31
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-06 16:26:16 +0200

    Add exercise.

--- Homeworks and Labs/ObjectsAndClasses/01.DayOfWeek/DayOfWeek.cs
+++ Homeworks and Labs/ObjectsAndClasses/01.DayOfWeek/DayOfWeek.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-class DayOfWeek
+public class DayOfWeek
 {
-    static void Main(string[] args)
+    public static void Main()
     {
         string dataAsStr = Console.ReadLine();
         DateTime date = DateTime.ParseExact(dataAsStr, "d-M-yyyy", CultureInfo.InvariantCulture);
@@ -16,5 +11,3 @@ class DayOfWeek
         Console.WriteLine(dayOfWeek);
     }
 }
-
-
