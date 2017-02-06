4033b32cdcfb226a5a46b87ffc3859ed0d9a0a7b
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-06 16:27:09 +0200

    Add exercise.

--- Homeworks and Labs/ObjectsAndClasses/07.SalesReport/SalesReport.cs
+++ Homeworks and Labs/ObjectsAndClasses/07.SalesReport/SalesReport.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-class Sale
+public class Sale
 {
     public string Town { get; set; }
 
@@ -14,15 +11,34 @@ class Sale
     public decimal Price { get; set; }
 
     public decimal Quantity { get; set; }
+}
+
+public class SalesReport
+{
+    public static void Main()
+    {
+        List<Sale> sales = ReadListOfSales();
+
+        List<string> towns = sales.Select(s => s.Town).OrderBy(t => t).Distinct().ToList();
+
+        foreach (var town in towns)
+        {
+            decimal salesForTown = sales.Where(s => s.Town == town).Sum(s => s.Price * s.Quantity);
+
+            Console.WriteLine($"{town} -> {salesForTown:f2}");
+        }
+    }
 
     public static List<Sale> ReadListOfSales()
     {
         int numOfSales = int.Parse(Console.ReadLine());
+
         List<Sale> sales = new List<Sale>();
 
         for (int i = 0; i < numOfSales; i++)
         {
             string[] saleInfo = Console.ReadLine().Split().ToArray();
+
             Sale sale = new Sale
             {
                 Town = saleInfo[0],
@@ -30,26 +46,10 @@ class Sale
                 Price = decimal.Parse(saleInfo[2]),
                 Quantity = decimal.Parse(saleInfo[3])
             };
+
             sales.Add(sale);
         }
+
         return sales;
     }
 }
-    
-class SalesReport
-{
-    static void Main(string[] args)
-    {
-
-        List<Sale> sales = Sale.ReadListOfSales();
-
-        List<string> towns = sales.Select(s => s.Town).OrderBy(t => t).Distinct().ToList();
-
-        foreach (var t in towns)
-        {
-            decimal salesForTown = sales.Where(s => s.Town == t).Sum(s => s.Price * s.Quantity);
-            Console.WriteLine($"{t} -> {salesForTown:f2}");
-        }
-    }
-}
-
