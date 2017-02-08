f5d4ffc240f0c9c9ef757fe8f3867907a5d67e2c
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-08 18:06:38 +0200

    Add classes.

---
+++ Homeworks and Labs/ObjectsAndClasses-Exercises/10.StudentGroups/Group.cs
@@ -0,0 +1,11 @@
+﻿namespace _10.StudentGroups
+{
+    using System.Collections.Generic;
+
+    public class Group
+    {
+        public Town Town { get; set; }
+
+        public List<Student> Students { get; set; }
+    }
+}
