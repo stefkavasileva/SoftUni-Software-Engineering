f556ef4d5e590d016c50208138256012cdf7397a
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-08 17:33:29 +0200

    Add class.

---
+++ Homeworks and Labs/ObjectsAndClasses-Exercises/09.TeamworkProjects/Team.cs
@@ -0,0 +1,20 @@
+﻿namespace _09.TeamworkProjects
+{
+    using System.Collections.Generic;
+
+    public class Team
+    {
+        public Team(string name, string creator, List<string> members)
+        {
+            this.Name = name;
+            this.Creator = creator;
+            this.Members = members;
+        }
+
+        public string Name { get; set; }
+
+        public string Creator { get; set; }
+
+        public List<string> Members { get; set; }
+    }
+}
