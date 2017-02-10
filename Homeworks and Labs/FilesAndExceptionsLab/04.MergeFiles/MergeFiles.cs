efc53a5bbf50f276cb0a87022bdcefd4ded2e702
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-10 14:26:06 +0200

    Add exercise.

---
+++ Homeworks and Labs/FilesAndExceptionsLab/04.MergeFiles/MergeFiles.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.MergeFiles
+{
+    using System;
+    using System.IO;
+
+    public class MergeFiles
+    {
+        public static void Main()
+        {
+            var firstFile = File.ReadAllLines("firstInput.txt");
+            var secondFile = File.ReadAllLines("secondInput.txt");
+
+            File.WriteAllText("result.txt", string.Empty);
+
+            for (int i = 0; i < firstFile.Length; i++)
+            {
+                File.AppendAllText("result.txt", firstFile[i] + Environment.NewLine + secondFile[i] + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ Homeworks and Labs/FilesAndExceptionsLab/05.FolderSize/FolderSize.cs
@@ -0,0 +1,24 @@
+﻿namespace _05.FolderSize
+{
+    using System.IO;
+
+    public class FolderSize
+    {
+        public static void Main()
+        {
+            var files = Directory.GetFiles("TestFolder");
+
+            double sum = 0;
+
+            foreach (string file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText("оutput.txt", sum.ToString());
+        }
+    }
+}
