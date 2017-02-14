4914791957be5cec75031e74506034657f6f0886
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-14 15:16:14 +0200

    Add exercises.

---
+++ Homeworks and Labs/Strings-Lab/03.TextFilter/TextFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class TextFilter
+{
+    public static void Main()
+    {
+        var bannedWords = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var text = Console.ReadLine();
+
+        foreach (var word in bannedWords)
+        {
+            text = text.Replace(word, new string('*', word.Length));
+        }
+
+        Console.WriteLine(text);
+    }
+}
