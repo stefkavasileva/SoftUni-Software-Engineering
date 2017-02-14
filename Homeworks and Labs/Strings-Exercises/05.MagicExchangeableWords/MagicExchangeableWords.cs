994c25d22c517e6073895a56edff55580eda3c87
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-14 18:36:02 +0200

    Add exercises.

---
+++ Homeworks and Labs/Strings-Exercises/05.MagicExchangeableWords/MagicExchangeableWords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MagicExchangeableWords
+{
+    public static void Main()
+    {
+        var inputLine = Console.ReadLine().Split();
+        var firstString = inputLine[0];
+        var secondString = inputLine[1];
+
+        bool isExchangeble = IsExchangeble(firstString, secondString);
+
+        Console.WriteLine(isExchangeble.ToString().ToLower());
+
+    }
+
+    private static bool IsExchangeble(string firstString, string secondString)
+    {
+        var letters = new Dictionary<char, char>();
+
+        for (int i = 0; i < firstString.Length; i++)
+        {
+            var firstWordLetter = firstString[i];
+            var secondWordLetter = secondString[i];
+
+            if (!letters.ContainsKey(firstWordLetter))
+            {
+                letters.Add(firstWordLetter, secondWordLetter);
+            }
+            else if (!letters[firstWordLetter].Equals(secondWordLetter))
+            {
+                return false;
+            }
+        }
+              
+        return true;
+    }
+}
+
---
+++ Homeworks and Labs/Strings-Exercises/09.MelrahShake/MelrahShake.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+public class MelrahShake
+{
+    public static void Main()
+    {
+        var randomCharacters = Console.ReadLine();
+        var pattern = Console.ReadLine();
+
+        var messages = new StringBuilder();
+
+        if (pattern.Length == 0)
+        {
+            Console.WriteLine("No shake.");
+            Console.WriteLine(randomCharacters);
+            return;
+        }
+
+        while (true)
+        {
+            var firstOccureence = randomCharacters.IndexOf(pattern);
+            var lastOccureence = randomCharacters.LastIndexOf(pattern);
+
+            if (firstOccureence > -1 && lastOccureence > -1 && pattern.Length > 0)
+            {
+                randomCharacters = randomCharacters.Remove(lastOccureence, pattern.Length);
+                randomCharacters = randomCharacters.Remove(firstOccureence, pattern.Length);
+                messages.AppendLine("Shaked it.");
+
+                pattern = pattern.Remove(pattern.Length / 2, 1);
+
+            }
+            else
+            {
+                messages.AppendLine("No shake.");
+                break;
+            }
+
+        }
+
+        Console.Write(messages.ToString());
+        Console.WriteLine(randomCharacters);
+
+    }
+}
+
