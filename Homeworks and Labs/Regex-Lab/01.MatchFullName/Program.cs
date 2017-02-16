1bce8ecfc0656f37a2484c2563f44e53f2229a6b
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-16 18:26:15 +0200

    Add Regex-Lab

---
+++ Homeworks and Labs/Regex-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main()
+    {
+        var pattren = @"^[A-Z]{1}[a-z]+ [A-Z][a-z]+$";
+
+        var names = Console.ReadLine().Split(',');
+
+        foreach (var name in names)
+        {
+            Regex reg = new Regex(pattren);
+
+            if (reg.IsMatch(name.Trim()))
+            {
+                Console.WriteLine(name.Trim());
+            }
+        }
+    }
+}
---
+++ Homeworks and Labs/Regex-Lab/03.ReplaceTag/ReplaceTag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class ReplaceTag
+{
+    public static void Main()
+    {
+        //66/100
+        var result = new List<string>();
+
+        var pattern = @"<a.*href=(""|')(.*)\1>(.*?)<\/a>";
+        var inputLine = Console.ReadLine();
+
+        var reg = new Regex(pattern);
+        var matches = reg.Matches(inputLine);
+
+        inputLine = reg.Replace(inputLine, @"[URL href=""$2""]$3[/URL]");
+
+        Console.WriteLine(inputLine);
+    }
+}
+
