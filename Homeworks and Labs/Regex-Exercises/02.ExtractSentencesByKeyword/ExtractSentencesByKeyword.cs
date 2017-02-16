1cdd07d38b9f5b0fb0353b9c08fe947422929095
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-16 18:25:51 +0200

    Add Regex-Exercises

---
+++ Homeworks and Labs/Regex-Exercises/02.ExtractSentencesByKeyword/ExtractSentencesByKeyword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class ExtractSentencesByKeyword
+{
+    public static void Main()
+    {
+        var inputPattern = Console.ReadLine().Trim();
+        var text = Console.ReadLine().Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var pattern = "\\b" + inputPattern + "\\b";
+
+        var reg = new Regex(pattern);
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (reg.IsMatch(text[i]))
+            {
+                Console.WriteLine(text[i].Trim());
+            }
+        }
+    }
+}
