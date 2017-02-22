8bdafc13131fc340c1cfce3773514534648e5170
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-22 11:28:54 +0200

    Update code.

--- OldExams/ExamI-October2016/04.Files/Files.cs
+++ OldExams/ExamI-October2016/04.Files/Files.cs
@@ -6,6 +6,61 @@ public class Files
 {
     public static void Main()
     {
+        var files = new Dictionary<string, Dictionary<string, long>>();
 
+        var filesCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < filesCount; i++)
+        {
+            var filePathArgs = Console.ReadLine()
+                .Split(new[] { '\\' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var root = filePathArgs.First().Trim();
+
+            var fileArgs = filePathArgs
+                .Last()
+                .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var name = fileArgs.First().Trim();
+            var size = long.Parse(fileArgs.Last().Trim());
+
+            if (!files.ContainsKey(root))
+            {
+                files.Add(root, new Dictionary<string, long>());
+            }
+
+            if (!files[root].ContainsKey(name))
+            {
+                files[root].Add(name, 0);
+            }
+
+            files[root][name] = size;
+        }
+
+        var filesInGivenRoot = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var extension = filesInGivenRoot.First().Trim();
+        var givenRoot = filesInGivenRoot.Last().Trim();
+
+        bool hasFile = false;
+
+        if (files.ContainsKey(givenRoot))
+        {
+            var currentFiles = files[givenRoot].OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+
+            foreach (var file in currentFiles)
+            {
+                if (file.Key.Contains($".{extension}"))
+                {
+                    Console.WriteLine($"{file.Key} - {file.Value} KB");
+                    hasFile = true;
+                }
+            }
+        }
+
+        if (!hasFile)
+        {
+            Console.WriteLine("No");
+        }
     }
 }
