225bbbdfd383fcd2547a7d14ee8c04be6c2460fd
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-24 19:31:11 +0200

    Add exercises.

---
+++ Progr.Fund  Extended/Debugging-Exercises/01.TrickyStrings/TrickyStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class TrickyStrings
+{
+    public static void Main()
+    {
+        var delimiter = Console.ReadLine();
+        var numberOfStrings = int.Parse(Console.ReadLine());
+
+        var result = string.Empty;
+
+        for (int i = 0; i < numberOfStrings; i++)
+        {
+            if (i < numberOfStrings - 1)
+            {
+                result += Console.ReadLine() + delimiter;
+                continue;
+            }
+
+            result += Console.ReadLine();
+        }
+
+        Console.WriteLine(result);
+    }
+}
