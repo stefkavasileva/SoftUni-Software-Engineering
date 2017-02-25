cf01c1088f7324a7a0147296af01ae3018e57b1f
Stefka Vasileva <stefkavasileva93@gmail.com>
2017-02-25 17:35:14 +0200

    Add exercises.

---
+++ Progr.Fund  Extended/Lists-Processing-Lab/05.SortNumbers/SortNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SortNumbers
+{
+    public static void Main()
+    {
+        List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+        numbers.Sort();
+
+        Console.WriteLine(string.Join(" <= ", numbers));
+    }
+}
---
+++ Progr.Fund  Extended/Lists-Processing-Lab/07.CountNumbers/CountNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CountNumbers
+{
+    public static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        SortedDictionary<int, int> numbersCount = new SortedDictionary<int, int>();
+
+        foreach (var number in numbers)
+        {
+            if (!numbersCount.ContainsKey(number))
+            {
+                numbersCount.Add(number, 0);
+            }
+
+            numbersCount[number]++;
+        }
+
+        foreach (var number in numbersCount)
+        {
+            Console.WriteLine($"{number.Key} -> {number.Value}");
+        }
+    }
+}
