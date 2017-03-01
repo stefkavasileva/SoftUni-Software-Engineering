4b5ed126090250c666938d72508aac3a8f169ba1
stefkavasileva <stefkavasileva@gmail.com>
2017-03-01 13:04:46 -0800

    Add exercises.

---
+++ Progr.Fund  Extended/List-MoreExercises/01.DistinctList/DistinctList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DistinctList
+{
+    public static void Main()
+    {
+        var numbers = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+        Console.WriteLine(string.Join(" ",numbers));
+    }
+}
+
