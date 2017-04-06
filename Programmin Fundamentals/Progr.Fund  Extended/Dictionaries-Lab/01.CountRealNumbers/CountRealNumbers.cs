a6a0e5ea2f8e837684b4ccab1a1b13389499f631
stefkavasileva <stefkavasileva93@gmail.com>
2017-04-06 14:14:41 +0300

    Add Dictionaries-Lab and README.md

---
+++ Programmin Fundamentals/Progr.Fund  Extended/Dictionaries-Lab/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CountRealNumbers
+{
+    public static void Main()
+    {      
+        var realNums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+        var numsCount = new SortedDictionary<double,int>();
+
+        foreach (var number in realNums)
+        {
+            if (!numsCount.ContainsKey(number))
+            {              
+                numsCount.Add(number,0);   
+            }
+
+            numsCount[number]++;
+        }
+
+        foreach (var kvp in numsCount)
+        {
+            Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+        }
+    }
+}
---
+++ Programmin Fundamentals/Progr.Fund  Extended/Dictionaries-Lab/02.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class OddOccurrences
+{
+    public static void Main()
+    {
+        var words = Console.ReadLine()
+            .Split()
+            .Select(w => w.ToLower())
+            .ToArray();
+
+        var wordsCount = new Dictionary<string, int>();
+
+        foreach (var word in words)
+        {
+            if (!wordsCount.ContainsKey(word))
+            {
+                wordsCount.Add(word, 0);
+            }
+
+            wordsCount[word]++;
+        }
+
+        var wordsWithOddCount = wordsCount
+            .Where(w => w.Value % 2 == 1)
+            .ToDictionary(w => w.Key, w => w.Value)
+            .Keys
+            .ToList();
+
+        Console.WriteLine(string.Join(", ", wordsWithOddCount));
+    }
+}
