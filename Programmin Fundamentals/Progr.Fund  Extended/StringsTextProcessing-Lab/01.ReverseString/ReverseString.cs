aa700e7d055c4ee45fb047ed60238929ab1e3676
stefkavasileva <stefkavasileva93@gmail.com>
2017-04-19 20:21:44 +0300

    Add StringsTextProcessing-Lab.sln

---
+++ Programmin Fundamentals/Progr.Fund  Extended/StringsTextProcessing-Lab/01.ReverseString/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public class ReverseString
+{
+    public static void Main()
+    {
+        var inputString = Console.ReadLine();
+        var reversedString = string.Join(string.Empty, inputString.ToCharArray().Reverse());
+        Console.WriteLine(reversedString);
+    }
+}
---
+++ Programmin Fundamentals/Progr.Fund  Extended/StringsTextProcessing-Lab/02.CountSubstrOccurrences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class CountSubstOccurrences
+{
+    public static void Main()
+    {
+        var text = Console.ReadLine().ToLower();
+        var pattern = Console.ReadLine().ToLower();
+
+        var index = 0;
+        var count = 0;
+
+        while (index > -1)
+        {
+            index = text.IndexOf(pattern, index + 1);
+
+            if (index > -1)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
