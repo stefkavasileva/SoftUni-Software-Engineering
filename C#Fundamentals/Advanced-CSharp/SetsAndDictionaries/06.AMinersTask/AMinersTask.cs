6f7a7162b4e6747490018602d1059a58cd4e2b8a
stefkavasileva <stefkavasileva93@gmail.com>
2017-05-07 19:04:15 +0300

    Add exercises

---
+++ C#Fundamentals/Advanced-CSharp/SetsAndDictionaries/06.AMinersTask/AMinersTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class AMinersTask
+{
+    public static void Main()
+    {
+        var resourses = new Dictionary<string, int>();
+
+        while (true)
+        {
+            var resourse = Console.ReadLine();
+            if (resourse.Equals("stop")) break;
+            var quantity = int.Parse(Console.ReadLine());
+
+            if (!resourses.ContainsKey(resourse))
+            {
+                resourses.Add(resourse, 0);
+            }
+
+            resourses[resourse] += quantity;
+        }
+
+        foreach (var kvp in resourses)
+        {
+            Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+        }
+    }
+}
---
+++ C#Fundamentals/Advanced-CSharp/SetsAndDictionaries/07.FixEmails/FixEmails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FixEmails
+{
+    public static void Main()
+    {
+        var contacts = new Dictionary<string, string>();
+
+        while (true)
+        {
+            var name = Console.ReadLine();
+            if (name.Equals("stop")) break;
+            var email = Console.ReadLine();
+
+            if(!email.Contains(".uk") && !email.Contains(".us"))
+            {
+                if (contacts.ContainsKey(name))
+                {
+                    contacts.Add(name, string.Empty);
+                }
+
+                contacts[name] = email;
+            }
+        }
+
+        foreach (var contact in contacts)
+        {
+            Console.WriteLine($"{contact.Key} -> {contact.Value}");
+        }
+    }
+}
