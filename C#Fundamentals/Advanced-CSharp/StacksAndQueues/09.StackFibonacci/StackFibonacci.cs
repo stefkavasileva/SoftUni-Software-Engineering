c9948a6af05642c40c0d801eaa13502d3e2c32cd
stefkavasileva <stefkavasileva93@gmail.com>
2017-05-07 12:59:40 +0300

    Add exercises, README.md and image

---
+++ C#Fundamentals/Advanced-CSharp/StacksAndQueues/09.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StackFibonacci
+{
+    public static void Main()
+    {
+        var n = long.Parse(Console.ReadLine());
+        if (n <= 1)
+        {
+            Console.WriteLine(n);
+            return;
+        }
+
+        var fib = new Queue<long>();
+        fib.Enqueue(1);
+        fib.Enqueue(1);
+        for (int i = 0; i < n-2; i++)
+        {
+            var num = fib.Dequeue() + fib.Peek();
+            fib.Enqueue(num);
+        }
+
+        fib.Dequeue();
+        Console.WriteLine(fib.Dequeue());
+    }
+}
+
