e7ba81e83acc89b63afe5c1ff8e84484ddaa0194
stefkavasileva <stefkavasileva93@gmail.com>
2017-05-08 15:28:32 +0300

    Add exercises

---
+++ C#Fundamentals/Advanced-CSharp/RegularExpressions/06.SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class SentenceExtractor
+{
+    public static void Main()
+    {
+        var reg = new Regex(@".+?[!\./?]");
+        var word = Console.ReadLine();
+        var text = Console.ReadLine();
+
+        foreach (Match match in reg.Matches(text))
+        {
+            if (match.Value.Contains(" " + word + " "))
+            {
+                Console.WriteLine(match.Value.Trim());
+            }
+        }
+    }
+}
+
