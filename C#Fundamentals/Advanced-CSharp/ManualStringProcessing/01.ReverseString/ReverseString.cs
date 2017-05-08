3e02dc8e3a1246e0bb82ead86e0a7623f2747c3b
stefkavasileva <stefkavasileva93@gmail.com>
2017-05-08 13:49:19 +0300

    Add exercises

---
+++ C#Fundamentals/Advanced-CSharp/ManualStringProcessing/01.ReverseString/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+public class ReverseString
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine();
+        //var output = new string(input.Reverse().ToArray()); - 90/100 - memory
+
+        var result = new StringBuilder();
+        for (int i = input.Length-1; i >=0 ; i--)
+        {
+            result.Append(input[i]);
+        }
+
+        Console.WriteLine(result);
+    }
+}
+
---
+++ C#Fundamentals/Advanced-CSharp/ManualStringProcessing/02.StringLength/StringLength.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class StringLength
+{
+    public static void Main()
+    {
+        var text = Console.ReadLine();
+
+        var newtext = text.Substring(0, Math.Min(20, text.Length));
+
+        if (newtext.Length != 20)
+        {
+            newtext += new string('*', 20 - text.Length);
+        }
+
+        Console.WriteLine(newtext);
+    }
+}
+
