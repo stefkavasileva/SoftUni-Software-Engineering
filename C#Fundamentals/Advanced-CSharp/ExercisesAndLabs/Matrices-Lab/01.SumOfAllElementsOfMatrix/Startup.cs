5aff54cfd8fd48d64aebfbe7309de6e2836e8888
stefkavasileva <stefkavasileva93@gmail.com>
2017-06-01 15:02:28 +0300

    Add exercises

---
+++ C#Fundamentals/Advanced-CSharp/ExercisesAndLabs/Matrices-Lab/01.SumOfAllElementsOfMatrix/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumOfAllElementsOfMatrix
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var matrixSizes = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+            var rows = matrixSizes.First();
+            var cols = matrixSizes.Last();
+
+            var matrix = new int[rows][];
+
+            for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+            {
+                matrix[rowIndex] = Console.ReadLine()
+                    .Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(matrix.Select(x => x.Sum()).Sum());
+            
+        }
+    }
+}
---
+++ C#Fundamentals/Advanced-CSharp/ExercisesAndLabs/Matrices-Lab/04.PascalTriangle/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace _04.PascalTriangle
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var rowsCount = int.Parse(Console.ReadLine());
+            var pascal = new long[rowsCount][];
+
+            for (int rowIndex = 0; rowIndex < rowsCount; rowIndex++)
+            {
+                pascal[rowIndex] = new long[rowIndex + 1];
+                pascal[rowIndex][0] = 1;
+                pascal[rowIndex][pascal[rowIndex].Length - 1] = 1;
+
+                for (int colIndex = 1; colIndex < pascal[rowIndex].Length - 1; colIndex++)
+                {
+                    pascal[rowIndex][colIndex] = pascal[rowIndex - 1][colIndex - 1] + pascal[rowIndex - 1][colIndex];
+                }
+            }
+
+            var result = new StringBuilder();
+            foreach (var row in pascal)
+            {
+                result.AppendLine(string.Join(" ", row));
+            }
+
+            Console.Write(result);
+        }
+    }
+}
