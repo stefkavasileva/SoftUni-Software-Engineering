37ad80a27d01735ddce7cbe4cd002c52310365a0
stefkavasileva <stefkavasileva93@gmail.com>
2017-06-12 14:34:35 +0300

    Add exercise

--- C#Fundamentals/Advanced-CSharp/Exams/Exam-19June2016/02.CubicsRube/Startup.cs
+++ C#Fundamentals/Advanced-CSharp/Exams/Exam-19June2016/02.CubicsRube/Startup.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.CubicsRube
 {
@@ -10,7 +7,37 @@ namespace _02.CubicsRube
     {
         public static void Main()
         {
+            var n = int.Parse(Console.ReadLine());
 
+            var sum = 0L;
+            var freeDimensions = (int)Math.Pow(n, 3);
+
+            var input = Console.ReadLine();
+            while (!input.Equals("Analyze"))
+            {
+                var inputNumbers = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var d1 = inputNumbers[0];
+                var d2 = inputNumbers[1];
+                var d3 = inputNumbers[2];
+                var num = inputNumbers[3];
+               
+                if (d1 < n && d2 < n && d3 < n && d1 >= 0 && d2 >= 0 && d3 >= 0)
+                {
+                    sum += num;
+                    if (num > 0)
+                    {
+                        freeDimensions--;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(freeDimensions);
         }
     }
 }
