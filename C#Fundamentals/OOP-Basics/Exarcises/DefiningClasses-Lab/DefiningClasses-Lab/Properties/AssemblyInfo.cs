d8c657d987464888cf84fc1e9ff42817ffff9a47
stefkavasileva <stefkavasileva93@gmail.com>
2017-06-28 16:07:36 +0300

    Add lab

---
+++ C#Fundamentals/OOP-Basics/Exarcises/DefiningClasses-Lab/DefiningClasses-Lab/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DefiningClasses-Lab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DefiningClasses-Lab")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("20c03c35-8a75-42de-b4c9-9cc57e2cf8a3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C#Fundamentals/OOP-Basics/Exarcises/DefiningClasses-Lab/DefiningClasses-Lab/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Startup
+{
+    public static void Main()
+    {
+        var accounts = new Dictionary<int, BankAccount>();
+
+        var inputLine = Console.ReadLine();
+        while (inputLine != "End")
+        {
+            var tokens = inputLine
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var currentComandName = tokens[0];
+            if (currentComandName.Equals("Create"))
+            {
+                CreateAccount(int.Parse(tokens[1]), accounts);
+            }
+            else if (currentComandName.Equals("Deposit"))
+            {
+                Deposit(tokens, accounts);
+            }
+            else if (currentComandName.Equals("Withdraw"))
+            {
+                Withdraw(tokens, accounts);
+            }
+            else if (currentComandName.Equals("Print"))
+            {
+                Print(int.Parse(tokens[1]),accounts);
+            }
+
+            inputLine = Console.ReadLine();
+        }
+    }
+
+    private static void Print(int id ,Dictionary<int, BankAccount> accounts)
+    {
+        if (!IsAccountExist(accounts, id))
+        {
+            Console.WriteLine("Account does not exist");
+            return;
+        }
+
+        Console.WriteLine(accounts[id]);
+    }
+
+    private static void Withdraw(string[] tokens, Dictionary<int, BankAccount> accounts)
+    {
+        if (!IsAccountExist(accounts, int.Parse(tokens[1])))
+        {
+            Console.WriteLine("Account does not exist");
+            return;
+        }
+
+        var currentAccount = accounts[int.Parse(tokens[1])];
+        var amount = double.Parse(tokens[2]);
+
+        if (currentAccount.Balance < amount)
+        {
+            Console.WriteLine("Insufficient balance");
+            return;
+        }
+
+        currentAccount.Balance -= amount;
+    }
+
+    private static void Deposit(string[] tokens, Dictionary<int, BankAccount> accounts)
+    {
+        if (!IsAccountExist(accounts, int.Parse(tokens[1])))
+        {
+            Console.WriteLine("Account does not exist");
+            return;
+        }
+
+        var deposit = double.Parse(tokens[2]);
+        var id = int.Parse(tokens[1]);
+        accounts[id].Balance += deposit;
+    }
+
+    private static bool IsAccountExist(Dictionary<int, BankAccount> accounts, int id)
+    {
+        return accounts.ContainsKey(id);
+    }
+
+    private static void CreateAccount(int accountId, Dictionary<int, BankAccount> accounts)
+    {
+        if (!accounts.ContainsKey(accountId))
+        {
+            var account = new BankAccount();
+            account.ID = accountId;
+            accounts.Add(accountId, account);
+            return;
+        }
+
+        Console.WriteLine($"Account already exists");
+    }
+}
+
