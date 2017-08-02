0934a5a674a27e983bc7f664654453a34c3447cb
stefkavasileva <stefkavasileva93@gmail.com>
2017-08-02 13:15:59 +0300

    Update

---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/01.CardSuit/CardSuit.cs
@@ -0,0 +1,7 @@
+﻿public enum CardSuit
+{
+    Clubs,
+    Diamonds,
+    Hearts,   
+    Spades
+}
---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/01.CardSuit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Startup
+{
+    public static void Main()
+    {
+        Console.WriteLine("Card Suits:");
+        foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+        {
+            Console.WriteLine($"Ordinal value: {(int)suit}; Name value: {suit}");
+        }
+    }
+}
+
