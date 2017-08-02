e20dc03fcfd0b73f9cc34f708cc64fbc81c4a455
stefkavasileva <stefkavasileva93@gmail.com>
2017-08-02 13:17:08 +0300

    Update

---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/09.TrafficLight/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Startup
+{
+    public static void Main()
+    {
+        IList<TrafficLightSignal> trafficLights = new List<TrafficLightSignal>();
+        var input = Console.ReadLine().Split();
+
+        foreach (string signal in input)
+        {
+            TrafficLightSignal currentTrafficLight;
+            Enum.TryParse(signal, out currentTrafficLight);
+            trafficLights.Add(currentTrafficLight);
+        }
+
+        var switchesCount = int.Parse(Console.ReadLine());
+
+        var result = new StringBuilder();
+        for (int i = 0; i < switchesCount; i++)
+        {
+            for (int j = 0; j < trafficLights.Count; j++)
+            {
+                var trafficLight = trafficLights[j];
+                trafficLight = SwitchLight(trafficLight);
+                trafficLights[j] = trafficLight;
+                result.Append($"{trafficLight} ");
+
+            }
+            result.AppendLine();
+        }
+
+        Console.WriteLine(result.ToString().Trim());
+    }
+
+    private static TrafficLightSignal SwitchLight(TrafficLightSignal trafficLight)
+    {
+        var enumLength = Enum.GetNames(typeof(TrafficLightSignal)).Length;
+        var currentIndex = (int)trafficLight;
+        trafficLight = (TrafficLightSignal)((currentIndex + 1) % enumLength);
+        return trafficLight;
+    }
+}
+
---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/09.TrafficLight/TrafficLightSignal.cs
@@ -0,0 +1,7 @@
+﻿public enum TrafficLightSignal
+{
+    Red,
+    Green,
+    Yellow
+}
+
