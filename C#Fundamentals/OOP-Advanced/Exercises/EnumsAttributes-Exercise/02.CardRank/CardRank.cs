d79dd83bd9a22fb85018954330b9ff3acdf0cb91
stefkavasileva <stefkavasileva93@gmail.com>
2017-08-02 13:16:36 +0300

    Update

---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/02.CardRank/CardRank.cs
@@ -0,0 +1,17 @@
+﻿public enum CardRank
+{
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+}
+
---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/02.CardRank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Startup
+{
+    public static void Main()
+    {
+        Console.WriteLine("Card Ranks:");
+        foreach (CardRank suit in Enum.GetValues(typeof(CardRank)))
+        {
+            Console.WriteLine($"Ordinal value: {(int)suit}; Name value: {suit}");
+        }
+    }
+}
+
