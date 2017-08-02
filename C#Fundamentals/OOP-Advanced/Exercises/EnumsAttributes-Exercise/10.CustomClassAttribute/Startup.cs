421bd63aea10134182a72d0253c5f0c8f89f777b
stefkavasileva <stefkavasileva93@gmail.com>
2017-08-02 13:17:32 +0300

    Update

---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/10.CustomClassAttribute/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+[Custom("Pesho", 3, "Used for C# OOP Advanced Course - Enumerations and Attributes.", "Pesho", "Svetlio")]
+
+public class Startup
+{
+    public static void Main()
+    {
+        CustomAttribute attribute = (CustomAttribute)typeof(Startup).GetCustomAttributes(false).FirstOrDefault();
+
+        var input = Console.ReadLine();
+
+        while (!input.Equals("END"))
+        {
+            Console.WriteLine(attribute.Print(input));
+
+            input = Console.ReadLine();
+        }
+    }
+}
+
---
+++ C#Fundamentals/OOP-Advanced/Exercises/EnumsAttributes-Exercise/10.CustomClassAttribute/TypeAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class CustomAttribute : Attribute
+{
+    private readonly string author;
+    private readonly int revision;
+    private readonly string description;
+    private readonly IList<string> reviewers;
+
+    public CustomAttribute(string author, int revision, string description, params string[] inputReviewers)
+    {
+        this.author = author;
+        this.revision = revision;
+        this.description = description;
+        this.reviewers = new List<string>();
+
+        foreach (string reviewer in inputReviewers)
+        {
+            this.reviewers.Add(reviewer);
+        }
+    }
+
+    public string Print(string element)
+    {
+        switch (element)
+        {
+            case "Author":
+                return $"Author: {this.author}";
+
+            case "Revision":
+                return $"Revision: {this.revision}";
+
+            case "Description":
+                return $"Class description: {this.description}";
+
+            case "Reviewers":
+                return $"Reviewers: {string.Join(", ", this.reviewers)}";
+        }
+
+        return string.Empty;
+    }
+}
