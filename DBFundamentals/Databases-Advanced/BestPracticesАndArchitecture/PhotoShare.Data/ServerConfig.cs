a6643e06879b6452ad43672f2ab2b2e30e1d95ac
stefkavasileva <stefkavasileva93@gmail.com>
2017-11-27 23:11:45 +0200

    remove file

--- DBFundamentals/Databases-Advanced/BestPracticesАndArchitecture/PhotoShare.Data/ServerConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PhotoShare.Data
-{
-    internal class ServerConfig
-    {
-        internal static string ConnectionString => "Server=.;Database=PhotoShare;Integrated Security=True;";
-    }
-}
