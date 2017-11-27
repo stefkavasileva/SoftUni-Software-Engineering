f0b29ebd9b44cc5a3bf0bafe3d3ebaf1c06c183c
stefkavasileva <stefkavasileva93@gmail.com>
2017-11-27 23:14:22 +0200

    Add classes

---
+++ DBFundamentals/Databases-Advanced/BestPracticesАndArchitecture/PhotoShare.Client/Utilities/ErrorMessages.cs
@@ -0,0 +1,50 @@
+﻿namespace PhotoShare.Client.Utilities
+{
+    public static class ErrorMessages
+    {
+        public const string InvalidCommandName = "Command {0} not valid";
+
+        // Error messages for the AcceptFriend and AddFriend commands       
+        public const string NonExistentUser = "User {0} not found!";
+        public const string InvalidCredentials = "Invalid credentials!";
+        public const string UsersAreAlreadyFriends = "{0} is already a friend to {1}!";
+        public const string NotAddedAsFriends = "{0} has not added {1} as a friend!";
+
+        //Error messages for the AddTag and AddTagTo
+        public const string ExistingTag = "Tag {0} exists!";
+        public const string NonExistingTagOrAlbum = "Either tag or album do not exist!";
+
+        //Error messages for AddTownCommand
+        public const string ExistingTown = "Town {0} was already added!";
+
+        //Error messages for CreateAlbumCommand
+        public const string ExistingAlbum = "Album {0} exists!";
+        public const string NonExistingColor = "Color {0} not found!";
+        public const string InvalidTag = "Invalid tags!";
+
+        //Errror messages for the DeleteUserCommand
+        public const string UserIsAlreadyDeleted = "User {0} is already deleted!";
+
+        //Error messages for the LogingCommand
+        public const string InvalidUserOrPassword = "Invalid username or password!";
+
+        //Error messages for the LogoutCommand 
+        public const string LogoutNotAlowed = "You should log in first in order to logout.";
+
+        //Error messages for the ModifyUserCommand
+        public const string InvalidPropertyValue = "Value {0} not valid.{1}";
+        public const string InvalidPropertyName = "{0}Property {1} not supported!";
+        public const string InvalidPassword = "{0}Password must include a lower letter and a digit.{1}Invalid Password";
+        public const string InvalidTown = "{0}Town {1} not found!";
+        public const string InvalidProperty = "Property {0} not supported!";
+
+        //Error messages for the RegisterUserCommand
+        public const string AlreadyTakenUser = "Username {0} is already taken!";
+        public const string PasswordDontMatch = "Passwords do not match!";
+
+        //Error messages for the ShareAlbumCommand
+        public const string NonExistingAlbum = "Album {0} not found!";
+        public const string InvalidPermission = "Permission must be either “Owner” or “Viewer”!";
+
+    }
+}
---
+++ DBFundamentals/Databases-Advanced/BestPracticesАndArchitecture/PhotoShare.Client/Utilities/Messages.cs
@@ -0,0 +1,35 @@
+﻿namespace PhotoShare.Client.Utilities
+{
+    public static class Messages
+    {
+        public const string AcceptUserToFriendList = "{0} accepted {1} as a friend.";
+
+        public const string AddedUserToFriendList = "Friend {0} added to {1}";
+
+        public const string AddedTag = "Tag {0} was added successfully to database!";
+
+        public const string AddedTagToAlbum = "Tag {0} added to {1}!";
+
+        public const string AddedTown = "{0} was added to database!";
+
+        public const string CreatedAlbum = "Album {0} successfully created!";
+
+        public const string DeletedUser = "User {0} was deleted from the database!";
+
+        public const string Bye = "Good Bye!";
+
+        public const string NoFriendsMessage = "No friends for this user. :(";
+
+        public const string LoggedUser = "User {0} successfully logged in!";
+
+        public const string SuccessfullyLogout = "User {0} successfully logged out!";
+
+        public const string ModifiedUser = "User {0} {1} is {2}.";
+
+        public const string RegisteredUser = "User {0} was registered successfully!";
+
+        public const string SharedAlbum = "Username {0} added to album {1} ({2})";
+
+        public const string AddedPicture = "Picture {0} added to {albumName}!";
+    }
+}
