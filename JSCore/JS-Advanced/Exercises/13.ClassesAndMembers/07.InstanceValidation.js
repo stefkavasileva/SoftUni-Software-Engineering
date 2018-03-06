99887c35fc829f8c3b15e95f51343b27a42906d4
stefkavasileva <stefkavasileva93@gmail.com>
2018-03-06 19:50:59 +0200

    add exercises

---
+++ JSCore/JS-Advanced/Exercises/13.ClassesAndMembers/07.InstanceValidation.js
@@ -0,0 +1,9 @@
+class CheckingAccount {
+    constructor(clientId,email,firstName,lastName){
+        this.clientId = clientId;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.products = [];
+    }
+}
