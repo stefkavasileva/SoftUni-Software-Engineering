5d68a652e40439af7b1ad39bab447b3a4dd6bf5c
stefkavasileva <stefkavasileva93@gmail.com>
2018-05-21 18:48:27 +0300

    add exercises

---
+++ C#Web/C#WebDevelopmentBasics/Exercises/03.LabHTTPProtocol/01.URLDecode/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace _01.URLDecode
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var encodedUrl = Console.ReadLine();
+            var decodeUrl = WebUtility.UrlDecode(encodedUrl);
+
+            Console.WriteLine(decodeUrl);
+        }
+    }
+}
---
+++ C#Web/C#WebDevelopmentBasics/Exercises/03.LabHTTPProtocol/02.ValidateURL/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02.ValidateURL
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var encodedUrl = Console.ReadLine();
+            var decodeUrl = WebUtility.UrlDecode(encodedUrl);
+
+            var urlReg = new Regex(@"(http[s]*:\/\/)*(\w+\.\w+):*(\d+)*\/(\w+\/*)*\?*(\w+=\w+&*)*#*(\w+)*");
+            var urlMatch = urlReg.Match(decodeUrl);
+            if (!urlMatch.Success)
+            {
+                Console.WriteLine("Invalid URL");
+                return;
+            }
+
+            var protocol = urlMatch.Groups[1].Value.Trim(new char[] { '/', ':' });
+            var host = urlMatch.Groups[2].Value;
+            var port = urlMatch.Groups[3].Value;
+            if((protocol == "http" && port == "443") || (protocol == "https" && port == "80"))
+            {
+                Console.WriteLine("Invalid URL");
+                return;
+            }
+
+            var path = urlMatch.Groups[4].Value;
+            var queryStrings = string.Join("", urlMatch.Groups[5].Captures);
+            var fragment = urlMatch.Groups[6].Value;
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Protocol: {protocol}");
+            sb.AppendLine($"Host: {host}");
+            if (port != "") sb.AppendLine($"Port: {port}");
+            if (path == "") path = "/";
+            sb.AppendLine($"Path: {path}");
+            if (queryStrings != "") sb.AppendLine($"Query: {queryStrings}");
+            if (fragment != "") sb.AppendLine($"Fragment: {fragment}");
+
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
