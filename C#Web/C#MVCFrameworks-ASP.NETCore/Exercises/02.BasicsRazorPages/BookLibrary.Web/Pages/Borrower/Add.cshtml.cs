83ba38fdc220fecb8b964fb6da5ef9bc6c9ba9f9
stefkavasileva <stefkavasileva93@gmail.com>
2018-07-13 22:44:02 +0300

    add borrower

---
+++ C#Web/C#MVCFrameworks-ASP.NETCore/Exercises/02.BasicsRazorPages/BookLibrary.Web/Pages/Borrower/Add.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using BookLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookLibrary.Web.Pages.Borrower
+{
+    public class AddModel : PageModel
+    {
+        private BookLibraryContext _context;
+
+        public AddModel(BookLibraryContext context)
+        {
+            this._context = context;
+        }
+
+        [BindProperty]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public string Address { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (ModelState.IsValid)
+            {
+                var borrower = new BookLibrary.Models.Borrower
+                {
+                    Name = this.Name,
+                    Address = this.Address
+                };
+
+                this._context.Borrowers.Add(borrower);
+            }
+
+            return this.Page();
+        }
+    }
+}
